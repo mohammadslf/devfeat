c346319cf73222d364da4d32118829fb75b5eb35
mohammadslf <46641680+mohammadslf@users.noreply.github.com>
2021-02-19 18:14:03 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Page'),
     );
   }
 }
